5168003c1d1d10aec75c3c68caa2f5e90ab38efa
Richard <richardchoi54@gmail.com>
2024-10-02 18:23:58 -0400

    add cat card component

--- components/CatCard.tsx
+++ components/CatCard.tsx
@@ -8,15 +8,17 @@ import {
 } from "./ui/card";
 import { ICatData } from "api/getData";
 
-export default function CatCard({catData}:{catData: ICatData}) {
+export default function CatCard({ catData }: { catData: ICatData }) {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>{catData.breed}</CardTitle>
-        <CardDescription>{catData.gallery}</CardDescription>
+        <CardTitle>
+          {catData.breed} "({catData.latinName})"
+        </CardTitle>
+        <CardDescription>{catData.bodySizeAndShape}</CardDescription>
       </CardHeader>
       <CardContent>
-        <img src = {catData.gallery} />
+        <img src={`../src/public${catData.gallery}`} />
       </CardContent>
       <CardFooter>
         <p>Card Footer</p>
