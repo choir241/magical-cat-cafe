a768b88754361800015e50155a6c1feb4c730da5
Richard <richardchoi54@gmail.com>
2024-10-04 13:48:54 -0400

    install appwrite and setup client

---
+++ api/appwrite.tsx
@@ -0,0 +1,9 @@
+import { Client, Databases, Account } from "appwrite";
+
+const client = new Client();
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(import.meta.env.VITE_PROJECT_ID);
+
+export const account = new Account(client);
+export const databases = new Databases(client);
